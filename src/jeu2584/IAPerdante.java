b0f7ee1a687e4c411b5b8d6a7ea856c5b71a9ec9
ColletMathieu <matt.ob@laposte.net>
2018-04-13 23:26:30 +0200

    IA aléa et IA perdante

--- src/jeu2584/IAPerdante.java
+++ src/jeu2584/IAPerdante.java
@@ -16,9 +16,49 @@ public class IAPerdante extends Grille{
     }
     
     public void tourIA(){}{
-    
+        Grille fils1 = this;
+        Grille fils2 = this;
+        Grille fils3 = this;
+        Grille fils4 = this;
+        fils1.lanceurDeplacerCases(HAUT);
+        fils2.lanceurDeplacerCases(BAS);
+        fils3.lanceurDeplacerCases(GAUCHE);
+        fils4.lanceurDeplacerCases(DROITE);
+        int min=this.comparer(fils1.getScore(),fils2.getScore(),fils3.getScore(),fils4.getScore());
+        boolean b2 = true;
+        if(min==fils1.getScore()){
+            b2=this.lanceurDeplacerCases(HAUT);
+        }
+        else if(min==fils2.getScore()){
+            b2=this.lanceurDeplacerCases(BAS);
+        }
+        else if(min==fils3.getScore()){
+            b2=this.lanceurDeplacerCases(GAUCHE);
+        }
+        else if(min==fils4.getScore()){
+            b2=this.lanceurDeplacerCases(DROITE);
+        }
+        else if (b2) {
+            boolean b = this.nouvelleCase();
+                if (!b) this.gameOver();
+            }
+        System.out.println(this);
+        if (this.getValeurMax()>=OBJECTIF) this.victory();
+        if (this.partieFinie()) this.gameOver();
     
     
     }
-    
+    public int comparer(int int1,int int2,int int3,int int4){
+        int min=int1;
+        if(int2<min){
+            min=int2;
+        }
+        if(int3<min){
+            min=int3;
+        }
+        if(int4<min){
+            min=int4;
+        }
+        return min;
+    }
 }
